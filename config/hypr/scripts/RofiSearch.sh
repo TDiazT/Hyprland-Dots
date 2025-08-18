62429e7b0752370cc541fcdc78cb645cfbd3c06e
Tomas Diaz <tomdtron@gmail.com>
2025-08-18 15:59:18 +0200

    rofi: General base changes

--- config/hypr/scripts/RofiSearch.sh
+++ config/hypr/scripts/RofiSearch.sh
@@ -7,8 +7,8 @@ config_file=$HOME/.config/hypr/UserConfigs/01-UserDefaults.conf
 
 # Check if the config file exists
 if [[ ! -f "$config_file" ]]; then
-    echo "Error: Configuration file not found!"
-    exit 1
+  echo "Error: Configuration file not found!"
+  exit 1
 fi
 
 # Process the config file in memory, removing the $ and fixing spaces
@@ -19,18 +19,19 @@ eval "$config_content"
 
 # Check if $term is set correctly
 if [[ -z "$Search_Engine" ]]; then
-    echo "Error: \$Search_Engine is not set in the configuration file!"
-    exit 1
+  echo "Error: \$Search_Engine is not set in the configuration file!"
+  exit 1
 fi
 
 # Rofi theme and message
 rofi_theme="$HOME/.config/rofi/config-search.rasi"
-msg='‼️ **note** ‼️ search via default web browser'
+# msg='‼️ **note** ‼️ search via default web browser'
 
 # Kill Rofi if already running before execution
 if pgrep -x "rofi" >/dev/null; then
-    pkill rofi
+  pkill rofi
 fi
 
 # Open Rofi and pass the selected query to xdg-open for Google search
-echo "" | rofi -dmenu -config "$rofi_theme" -mesg "$msg" | xargs -I{} xdg-open $Search_Engine
+echo "" | rofi -dmenu -config "$rofi_theme" | xargs -I{} xdg-open $Search_Engine
+
