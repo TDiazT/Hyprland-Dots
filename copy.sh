7193712033a9f32191711cc3caa68b9811de7ab7
Tomas Diaz <tomdtron@gmail.com>
2025-08-23 15:31:33 +0200

    copy.sh: Remove ROG setup

--- copy.sh
+++ copy.sh
@@ -226,9 +226,9 @@ ${MAGENTA} NOTE:${RESET}
 done
 
 # Check if asusctl is installed and add rog-control-center on Startup
-if command -v asusctl >/dev/null 2>&1; then
-  sed -i '/^\s*#exec-once = rog-control-center/s/^#//' config/hypr/UserConfigs/Startup_Apps.conf
-fi
+# if command -v asusctl >/dev/null 2>&1; then
+#   sed -i '/^\s*#exec-once = rog-control-center/s/^#//' config/hypr/UserConfigs/Startup_Apps.conf
+# fi
 
 # Check if blueman-applet is installed and add blueman-applet on Startup
 if command -v blueman-applet >/dev/null 2>&1; then
